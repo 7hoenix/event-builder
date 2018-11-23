a346d8e56cf7a124927bbbcdb8bca1f40743d7fa
7hoenix <jpholzmann@gmail.com>
2018-11-23 13:26:32 -0600

    Find dates for events randomly

--- lib/events.rb
+++ lib/events.rb
@@ -1,2 +1,22 @@
 module Events
+  def self.find_dates(random = lambda { |entries| entries.sample() }, days_desired)
+    result = []
+    entries = build_entries(days_desired)
+    entries.each_slice(30) { |approximate_month| result.push(random.call(approximate_month)) }
+    result
+  end
+
+  private
+
+  def self.build_entries(entries = [], current_day)
+    if current_day == 0
+      return entries.reverse
+    end
+    updated_entries = entries.concat([build_entry(current_day)])
+    build_entries(updated_entries, current_day - 1)
+  end
+
+  def self.build_entry(day)
+    (Time.now + 86_400 * day).strftime("%F")
+  end
 end
--- spec/events_spec.rb
+++ spec/events_spec.rb
@@ -3,8 +3,33 @@ require('./lib/events')
 RSpec.describe Events do
   describe ".find_dates" do
     it "finds a random date in the next month" do
-      dates = Events.find_dates()
-      expect(dates).to eq(2)
+      fake_sample = lambda { |entries| entries.first }
+
+      dates = Events.find_dates(fake_sample, 30)
+
+      expect(dates.length).to eq(1)
+      expect(dates[0]).to eq((Time.now + 86_400).strftime("%F"))
+    end
+
+    it "finds two dates" do
+      fake_sample = lambda { |entries| entries.first }
+
+      dates = Events.find_dates(fake_sample, 60)
+
+      expect(dates.length).to eq(2)
+      expect(dates[0]).to eq((Time.now + 86_400).strftime("%F"))
+      expect(dates[1]).to eq((Time.now + 86_400 * 31).strftime("%F"))
+    end
+
+    it "will take partial months" do
+      fake_sample = lambda { |entries| entries.first }
+
+      dates = Events.find_dates(fake_sample, 61)
+
+      expect(dates.length).to eq(3)
+      expect(dates[0]).to eq((Time.now + 86_400).strftime("%F"))
+      expect(dates[1]).to eq((Time.now + 86_400 * 31).strftime("%F"))
+      expect(dates[2]).to eq((Time.now + 86_400 * 61).strftime("%F"))
     end
   end
 end
